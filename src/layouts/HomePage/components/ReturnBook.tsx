a9f72d511e2e33afebbbad5bf2ebab3162df4660
unknown <doanquoc12c3>
2023-09-01 09:44:14 +0800

    add useEffect to fetch hompage data + add spinloading

--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import BookModel from "../../../models/BookModels";
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{book:BookModel}> = (props) => {
     return (
         <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
             <div className="text-center">
+                {props.book.img?
+                <img src={props.book.img}
+                width='151'
+                height='233'
+                alt='book'></img>
+                :    
                 <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
                     width='151'
                     height='233'
                     alt='book'></img>
+            }
+                
                 <h6 className="mt-2">
-                    Book
+                    {props.book.title}
                 </h6>
-                <p>Doan Quoc</p>
+                <p>{props.book.author}</p>
                 <a className="btn main-color text-white" href="#">
                     Reserve
                 </a>
